7f71da5f0e04e2d9f290cf182c67e1ed9a1bb6d1
Krystian Postek <krystian@postek.eu>
2023-05-29 14:00:36 +0200

    feat: update cli tool

--- py_encryptor/cli.py
+++ py_encryptor/cli.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import click
 
-from py_encryptor import Cryptor
+from py_encryptor.algorithms.base import BaseEncryptionAlgorithm
+from py_encryptor.utils.manager import AlgorithmsManager
 
 
 @click.group()
@@ -8,20 +11,37 @@ def cli():
     pass
 
 
+algs_names = [x.__name__ for x in AlgorithmsManager.available_algorithms]
+
+
+def get_alg_by_name(name: str):
+    for alg in AlgorithmsManager.available_algorithms:
+        if alg.__name__ == name:
+            return alg
+
+    raise ValueError()
+
+
 @cli.command()
-@click.option('--file', '-f', help='File to encrypt')
+@click.option('--alg', '-a', help="Encryption algorithm", type=click.Choice(algs_names))
+@click.option('--source', '-s', help='File to encrypt')
+@click.option('--target', '-t', help="File to store encrypted data")
 @click.option('--passwd', '-p', help='Password to encrypt file')
-def encrypt(file, passwd):
-    c = Cryptor(passwd, file)
-    c.encrypt_decrypt()
+def encrypt(alg, source, target, passwd):
+    c = get_alg_by_name(alg)(passwd, Path(source))
+    c.encrypt(Path(target))
+    print("Done!")
 
 
 @cli.command()
-@click.option('--file', '-f', help='File to decrypt')
+@click.option('--alg', '-a', help="Encryption algorithm", type=click.Choice(algs_names))
+@click.option('--source', '-s', help='File to decrypt')
+@click.option('--target', '-t', help="File to store decrypted data")
 @click.option('--passwd', '-p', help='Password to decrypt file')
-def decrypt(file, passwd):
-    c = Cryptor(passwd, file)
-    c.decrypt()
+def decrypt(alg, source, target, passwd):
+    c = get_alg_by_name(alg)(passwd, Path(source))
+    c.decrypt(Path(target))
+    print("Done!")
 
 
 if __name__ == '__main__':
